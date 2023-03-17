371413cab9bac676541ecf3f75740a2ed02acd28
marypaduano <maria.paduano@gmail.com>
2023-03-17 18:27:44 +0100

    add TRIM in text area

--- badWord.php
+++ badWord.php
@@ -1,19 +1,25 @@
 <?php
 
 $words=$_GET['words'];
+$words_trim = trim($words);
 
-$words_length=strlen($words);
+
+$words_length=strlen($words_trim);
 
 
 $badword=$_GET['badword'];
+$badword_trim = trim($badword);
 
-$words2=str_replace($badword,"*****",$words);
 
-$words2_length=strlen($words2);
+$words2=str_replace($badword_trim,"*****",$words_trim);
+$words2_trim = trim($words2, "*****");
+
+
+$words2_length=strlen($words2_trim);
 
 ?>
 
-<h3>Questo è il paragrafo inserito: <?php echo $words ?></h3>
+<h3>Questo è il paragrafo inserito: <?php echo $words_trim ?></h3>
 <p>La lunghezza del paragrafo è di caratteri n: <?php echo $words_length ?></p>
 <h3>questo è il nuovo paragrafo con censura: <?php echo $words2 ?></h3>
 <p>La lunghezza del paragrafo censurato è di caratteri n: <?php echo $words2_length ?></p>
